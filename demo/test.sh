c62e5e28aef3f42b78fc2e550360e1c2a1ea1b98
Grant Griffiths <grant@portworx.com>
2020-07-23 01:39:04 -0700

    add command output for test
    Signed-off-by: Grant Griffiths <grant@portworx.com>

--- demo/test.sh
+++ demo/test.sh
@@ -7,6 +7,7 @@ exitFail() {
   kubectl -n kube-system describe deployment
   exit 1
 }
+set -x
 
 # pre-generated token added to secret
 token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXJAb3BlbnN0b3JhZ2UuaW8iLCJleHAiOjE3NTMxNDA0NDcsImdyb3VwcyI6WyIqIl0sImlhdCI6MTU5NTQ2MDQ0NywiaXNzIjoib3BlbnN0b3JhZ2UuaW8iLCJuYW1lIjoidXNlciIsInJvbGVzIjpbInN5c3RlbS51c2VyIl0sInN1YiI6InVzZXJAb3BlbnN0b3JhZ2UuaW8ifQ.41yebvGhSUlks4_perFh0sORmGnpulEML-7plFa0WWE
